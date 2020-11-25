23ec40f0d18a649fc37941ba1018b8eb1c429507
OblivionCreator <oblivioncreator@outlook.com>
2020-11-25 08:57:28 +0000

    Fixed Sans Detection

--- main.py
+++ main.py
@@ -1275,8 +1275,16 @@ async def _registerChar(ctx, user):
                     except:
                         await user.send("This character is too long to preview!")
                 elif selector in cfields:
+                    if selector.lower() == 'name':
+                        nameSelect = True
+                    else:
+                        nameSelect = False
                     await user.send("What would you like field `" + selector.capitalize() + "` to say?")
                     response = await getdm(ctx)
+
+                    if await canonCheck(response, user) and nameSelect is True:
+                        return
+
                     if response.lower() == 'exit':
                         await user.send("Exiting Character Creation!")
                         isRegistering = False
